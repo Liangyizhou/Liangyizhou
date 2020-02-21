248943ac423d7b3485afb50c9118e06c11a2ab01
Liangyizhou <461650578@qq.com>
2020-02-21 16:24:56 +0800

    1

--- printPrimeNumber/printPrimeNumber.cpp
+++ printPrimeNumber/printPrimeNumber.cpp
@@ -1,20 +1,36 @@
-﻿// printPrimeNumber.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include <stdlib.h>
 
-#include <iostream>
+using namespace std;
+//判断素数
+//返回值
+//1：素数
+//0：不是素数
+int checkPrimeNumber(int n) {
+	int i,flag=1;
+	for (i = 2; i < n; i = i+1) 
+	{
+		if (n % i == 0) {
+			flag = 0 ;
+			break;
+		}
+	}
+	return flag;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int i,totalnum = 100;
+	for(i = 2; i < totalnum+1; i = i+1)
+	{
+		//大于2的偶数肯定不是素数，在这把大于2的偶数筛掉
+		if (i % 2 == 0 && i > 2)
+			continue;
+		//判断数字
+		if (checkPrimeNumber(i)) {
+			cout << "The PrimeNumber is: " << i << endl;
+		}
+	}
+	system("pause");
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
